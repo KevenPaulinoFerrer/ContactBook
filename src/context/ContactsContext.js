07379f0260af579eb5bfbcc2a4942a0b58f49750
Keven Paulino <yadielpau712@gmail.com>
2026-03-18 20:09:16 -0400

    reverted to before basic implementation of mongodb logic

--- src/context/ContactsContext.js
+++ src/context/ContactsContext.js
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect, createContext } from 'react';
+import React, { useContext, useState, useEffect, createContext } from 'react';
 import {
   createContactRealm,
   readAllContacts,
